0edbd2f2af3f5ba863fde925711f9d960d9aac6b
Wgz <1808365012@mail.ynu.edu.cn>
2023-12-21 01:32:49 +0800

    完成用户主页

--- src/components/SpaceMain.vue
+++ src/components/SpaceMain.vue
@@ -1,26 +1,33 @@
 <script setup lang="ts">
-import type { Video } from '@/utils/api'
-import type { UserInfoCard } from '@/utils/getUserInfoCard'
+import type { Ref } from 'vue'
+import { UserApiService, type UserInfo, type Video, VideoApiService } from '../../generated'
 
 const route = useRoute()
 const mid = computed(() => Number.parseInt(`${route.params.mid}`))
-const { data: userInfoCard } = useDataWithAid<UserInfoCard, number>(mid, getUserInfoCard)
+const userInfo: Ref<UserInfo> = ref({})
+const videos: Ref<Video[]> = ref([])
 
-const { data: rcmdVideos, loadmore } = useDataLoadmore<Video>(getRcmdVideos)
-const videos = computed(() => rcmdVideos.value.filter(item => item.bvid))
+onMounted(async () => {
+  const res1 = await UserApiService.getUserInfoByUserIdUsingGet(mid.value)
+  if (res1.data !== undefined)
+    userInfo.value = res1.data
+  const res2 = await VideoApiService.getVideosByUserIdUsingGet(mid.value)
+  if (res2.data !== undefined)
+    videos.value = res2.data
+})
 </script>
 
 <template>
   <div class="flex flex-col items-center justify-center">
     <div class="flex items-end w-full h-40 p4 max-w-300">
-      <ElImage class="border-gray-400 border-solid rounded-full w-30 h-30 border-3 shrink-0" :src="userInfoCard?.card.face" referrerpolicy="no-referrer" />
+      <ElImage class="border-gray-400 border-solid rounded-full w-30 h-30 border-3 shrink-0" :src="userInfo?.avatar" referrerpolicy="no-referrer" />
       <div class="flex flex-col ml4">
-        <span class="text-xl">{{ userInfoCard?.card.name }}</span>
-        <span class="text-lg text-gray-600 opacity-40 line-clamp-1">{{ userInfoCard?.card.sign }}</span>
+        <span class="text-xl">{{ userInfo?.nick }}</span>
+        <span class="text-lg text-gray-600 opacity-40 line-clamp-1">{{ userInfo?.sign }}</span>
       </div>
     </div>
     <Divider class="w-full max-w-300" />
     <!-- Cannot get user video from space because anti-crawler so use Rcmd Video replace it -->
-    <VideosBoard class="h-90vh max-w-300" :callback="loadmore" :videos="videos" />
+    <VideosBoard class="h-90vh max-w-300" :videos="videos" />
   </div>
 </template>
