d802a86cac78aad34805f9392e43ada24fd00e38
davidlee <david.wushi@foxmail.com>
2023-12-22 09:26:20 +0800

    change package.json

--- env.d.ts
+++ env.d.ts
@@ -1,1 +1,10 @@
 /// <reference types="vite/client" />
+declare module 'tcplayer.js' {
+    function TCPlayer(
+        elementId: string,
+        options: {
+            sources: { src: string }[],
+            licenseUrl: string
+        }
+    ): any;
+}
